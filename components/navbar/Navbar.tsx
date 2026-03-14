9ee849e1b77885eba6c0e9d730de48f97389a81f
kayna <kaynanoliveira357@gmail.com>
2026-03-13 22:26:28 -0300

    adicionando hover

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -5,46 +5,46 @@ import { AvatarImage, Avatar, AvatarFallback } from "@/components/ui/avatar";
 export default function Navbar() {
   return (
     <header className="w-full">
-      <div className="mx-auto max-w-7xl px-8 h-25 flex items-center justify-between border-b-2 border-gray-200">
+      <div className="mx-auto max-w-6xl px-8 h-25 flex items-center justify-between border-b-2 border-gray-200">
         <div className="flex items-center gap-12">
           <Link href="/" className="shrink-0">
             <Image src="/images/logo.svg" alt="Logo" width={135} height={20} />
           </Link>
 
-          <nav className="hidden md:flex gap-8 text-gray-500 font-medium">
+          <nav className="flex gap-8 text-gray-500 font-medium h-25">
             <Link
               href="/features"
-              className="hover:text-black transition-colors"
+              className="h-full flex items-center border-b-4 border-transparent hover:text-black hover:border-orange-500 transition-all duration-300"
             >
               Collections
             </Link>
             <Link
               href="/features"
-              className="hover:text-black transition-colors"
+              className="h-full flex items-center border-b-4 border-transparent hover:text-black hover:border-orange-500 transition-all duration-300"
             >
               Men
             </Link>
             <Link
               href="/features"
-              className="hover:text-black transition-colors"
+              className="h-full flex items-center border-b-4 border-transparent hover:text-black hover:border-orange-500 transition-all duration-300"
             >
               Women
             </Link>
             <Link
               href="/features"
-              className="hover:text-black transition-colors"
+              className="h-full flex items-center border-b-4 border-transparent hover:text-black hover:border-orange-500 transition-all duration-300"
             >
               About
             </Link>
             <Link
               href="/features"
-              className="hover:text-black transition-colors"
+              className="h-full flex items-center border-b-4 border-transparent hover:text-black hover:border-orange-500 transition-all duration-300"
             >
               Contact
             </Link>
           </nav>
         </div>
-        <div className="flex items-center gap-6">
+        <div className="flex items-center gap-8">
           <Link href="/cart" className="relative">
             <Image
               src="/images/icon-cart.svg"
