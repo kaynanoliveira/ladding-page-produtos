f1dc839a72e267379a80aa611385347d03615411
kayna <kaynanoliveira357@gmail.com>
2026-03-24 19:29:56 -0300

    feat(constantes): extrai links e imagens para arquivos de dados

---
+++ components/constants/navigations.ts
@@ -0,0 +1,7 @@
+export const NAV_LINKS = [
+  { name: "Collections", href: "/collections" },
+  { name: "Men", href: "/men" },
+  { name: "Women", href: "/women" },
+  { name: "About", href: "/about" },
+  { name: "Contact", href: "/contact" },
+];
