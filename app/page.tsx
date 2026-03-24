a4317dc6d67db5dd26d7bdf13c99066e3bad7cf8
kayna <kaynanoliveira357@gmail.com>
2026-03-24 19:38:07 -0300

    refactor(projeto): atualiza caminhos de importação após reorganização

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/sections/Hero";
-import Navbar from "../components/navbar/Navbar";
+import Navbar from "../components/layout/Navbar";
 
 export default function Home() {
   return (
