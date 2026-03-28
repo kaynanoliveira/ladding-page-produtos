880b3ec792f83b22d59d3410f33ba956812ff66d
kayna <kaynanoliveira357@gmail.com>
2026-03-28 01:24:35 -0300

    feat(product): implementar troca de imagem principal ao clicar na miniatura

---
+++ components/constants/products2.ts
@@ -0,0 +1,6 @@
+export const IMAGES = [
+  "/images/image-product-1.jpg",
+  "/images/image-product-2.jpg",
+  "/images/image-product-3.jpg",
+  "/images/image-product-4.jpg",
+];
--- components/product/ProductGallery.tsx
+++ components/product/ProductGallery.tsx
@@ -1,11 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import { PRODUCT_IMAGES } from "@/components/constants/products";
+import { useState } from "react";
+import { IMAGES } from "../constants/products2";
 
 export default function ProductGallery() {
+  const [selectedImage, setSelectedImage] = useState(IMAGES[0]);
+
+  function atualizarImagemSelecionada(index: number) {
+    setSelectedImage(IMAGES[index]);
+  }
+
   return (
     <div className="flex flex-col items-center">
       <Image
-        src="/images/image-product-1.jpg"
+        src={selectedImage}
         alt="Hero Image"
         width={420}
         height={500}
@@ -21,6 +31,7 @@ export default function ProductGallery() {
             alt={`Thumbnail ${index + 1}`}
             width={80}
             height={90}
+            onClick={() => atualizarImagemSelecionada(index)}
             className="rounded-md object-cover border-3 border-transparent transition-all duration-300 hover:border-orange-500 hover:opacity-45 cursor-pointer"
           />
         ))}
